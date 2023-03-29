6c25cbfa39eb7d84a972cb18b4037b7068a66887
Jamie Benson <jamie_benson@live.com>
2023-03-29 19:44:49 +1030

    changed discount from 5 to 15

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -17,7 +17,7 @@ namespace Ticketing
         const decimal mdecBalcony = 55.5m;
         const decimal mdecGeneral = 48.75m;
         const decimal mdecBox = 82.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 15.0m; 
 
         private int Section
         {
